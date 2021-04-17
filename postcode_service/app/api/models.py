02f869de26c275f566c7098aa436441b7c4cf0c8
revliscano <revliscano@gmail.com>
2021-04-17 12:20:55 -0400

    clean up postcode_service skeleton

--- postcode_service/app/api/models.py
+++
@@ -1,7 +0,0 @@
-from decimal import Decimal
-from pydantic import BaseModel
-
-
-class LocationOut(BaseModel):
-    latitude: Decimal
-    longitude: Decimal
--- postcode_service/app/main.py
+++ postcode_service/app/main.py
@@ -4,11 +4,8 @@ import os
 sys.path.append(os.path.dirname(__file__))
 
 from fastapi import FastAPI
-from api.views import locations
-from database.setup import data_access_layer
+from api.views import postcodes
 
-
-data_access_layer.db_init()
 
 app = FastAPI()
-app.include_router(locations, prefix='/api/v1/locations', tags=['locations'])
+app.include_router(locations, prefix='/api/v1/postcodes', tags=['postcodes'])
